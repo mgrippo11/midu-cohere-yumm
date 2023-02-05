901db2b5668483f1c95b0d964e3da77ec3f43856
mgrippo11 <mgrippo_11@hotmail.com>
2023-02-04 22:00:15 -0300

    Fix: eviroment vars

--- src/hooks/GetMealIdea.jsx
+++ src/hooks/GetMealIdea.jsx
@@ -3,9 +3,6 @@ import ruleta from '../img/ruleta.png';
 import flecha from '../img/flecha.png';
 import recipeLogo from '../img/recipe.svg'
 
-const COHERE_API_KEY = 'SW291oCP7Zf34siESGiFAMlr3spDEnJM3UHL1M1B'
-const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
-
 const mealsArray = ['breakfast', 'lunch', 'afternoon snack', 'Dinner']
 const tipesMealsArray = ['Vegetarian', 'Vegan', 'Celiac']
 
@@ -24,11 +21,11 @@ const GetMealIdea = () => {
       setLoading(true)
       setSpin(true)
 
-      const response = await fetch(COHERE_API_GENERATE_URL, {
+      const response = await fetch(process.env.REACT_APP_COHERE_API_GENERATE_URL, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `BEARER ${COHERE_API_KEY}`
+          Authorization: `BEARER ${process.env.REACT_APP_COHERE_API_KEY}`
         },
         body: JSON.stringify({
           model: 'command-xlarge-20221108',
@@ -56,11 +53,11 @@ const GetMealIdea = () => {
       console.log(input)
       setLoadingR(true)
       setMeal(input)
-      const responseR = await fetch(COHERE_API_GENERATE_URL, {
+      const responseR = await fetch(process.env.REACT_APP_COHERE_API_GENERATE_URL, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `BEARER ${COHERE_API_KEY}`
+          Authorization: `BEARER ${process.env.REACT_APP_COHERE_API_KEY}`
         },
         body: JSON.stringify({
           model: 'command-xlarge-20221108',
